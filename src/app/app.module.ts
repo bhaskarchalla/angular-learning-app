5129e2c43eb9784bfbb740b6280cbb5f373acf76
Bhaskar Challa <challabhaskar91@gmail.com>
2023-02-06 13:26:09 +0530

    Removing unwanted code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { DirectiveExampleComponentComponent } from './directive-example-component/directive-example-component.component';
 import { InputformatDirective } from './inputformat.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DirectiveExampleComponentComponent,
     InputformatDirective
   ],
   imports: [
